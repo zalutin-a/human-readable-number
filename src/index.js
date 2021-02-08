82f0909e1473b2fea3d694189165f46b55f532b8
zalutin-a <an9rey89@yandex.ru>
2021-02-08 14:46:21 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+let arrOne = ['zero','one','two','three','four','five','six','seven','eight','nine',
+            'ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+let arrTwenty = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+
+function gNum(num){
+    let strNumb = String(num)
+    if(num < 20) {return arrOne[num]}
+    else {return arrTwenty[strNumb.charAt(0)] + (strNumb.charAt(1) == 0 ? '' : ' ' + arrOne[strNumb.charAt(1)])  }
+}
+
 module.exports = function toReadable (number) {
-    let arr = ['one','two','three','four','five','six','seven','eight','nine']
+    let strNumb = String(number)
+    let twoLustNum
+    if(strNumb.length >= 3){
+        twoLustNum = strNumb.charAt(1) == 0 ? '' + strNumb.charAt(2): strNumb.charAt(1) + strNumb.charAt(2)
+        return gNum(strNumb.charAt(0)) + ' hundred' + (twoLustNum == 00 ? '': ' ' + gNum(twoLustNum))
+    }
+    else{ return gNum(number)}
 }
